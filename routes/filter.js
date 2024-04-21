6808888bfb29d2075364b4f01179905b7f5a838c
William White, Jr <wewjr82@gmail.com>
2024-04-21 01:01:33 -0400

    forgot to commit out filter route

--- routes/filter.js
+++ routes/filter.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const postsController = require("../controllers/posts"); 
+// const express = require("express");
+// const router = express.Router();
+// const postsController = require("../controllers/posts"); 
 
-// Define route to handle GET requests to /filtered_posts
-router.get("/filtered_posts", postsController.filterPosts);
+// // Define route to handle GET requests to /filtered_posts
+// router.get("/filtered_posts", postsController.filterPosts);
 
-module.exports = router;
+// module.exports = router;
